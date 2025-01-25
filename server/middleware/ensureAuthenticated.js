1f989ed1add433879bdd45477bb547662e754d7b
mumuw2002 <chanya.tho@kkumail.com>
2025-01-26 03:27:14 +0700

    2 commit

---
+++ server/middleware/ensureAuthenticated.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        req.flash('error_msg', 'กรุณาล็อกอินเพื่อเข้าถึงหน้านี้');
+        res.redirect('/login');
+    }
+};
--- server/routes/spaceRoutes.js
+++ server/routes/spaceRoutes.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const spaceController = require('../controllers/spaceController');
+const { ensureAuthenticated } = require('../config/auth'); // นำเข้า middleware
 
-router.get('/space', spaceController.SpaceDashboard);
+router.get('/space', ensureAuthenticated, spaceController.SpaceDashboard);
 
 module.exports = router;
