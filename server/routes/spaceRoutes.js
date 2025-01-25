da2350f4d9bc57c55653ebbb48c1d51286af3c1b
mumuw2002 <chanya.tho@kkumail.com>
2025-01-26 03:30:50 +0700

    2 commit

--- server/routes/spaceRoutes.js
+++ server/routes/spaceRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const spaceController = require('../controllers/spaceController');
-const { ensureAuthenticated } = require('../config/auth'); // นำเข้า middleware
+const { ensureAuthenticated } = require('../config/ensureAuthenticated'); // นำเข้า middleware
 
 router.get('/space', ensureAuthenticated, spaceController.SpaceDashboard);
 
