34f338a6179be1d960d204c4f705d087423d6454
mumuw2002 <chanya.tho@kkumail.com>
2025-01-26 01:22:44 +0700

    first commit

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 
 require('./server/config/passport');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -19,6 +20,14 @@ const port = process.env.PORT || 5001;
 // Middleware สำหรับ parse body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Set Content Security Policy header
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", 
+      "default-src 'none'; img-src 'self' data:; script-src 'self'; style-src 'self';");
+  next();
+});
+
 
 // เชื่อมต่อ MongoDB
 connectDB()
@@ -30,44 +39,38 @@ connectDB()
         process.exit(1);
     });
 
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-        cookie: {
+    app.use(
+        session({
+          secret: process.env.SESSION_SECRET,
+          resave: false,
+          saveUninitialized: false,
+          store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), 
+          cookie: {
             secure: process.env.NODE_ENV === 'production',
             httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000, // 24 hours
+            maxAge: 24 * 60 * 60 * 1000,
             sameSite: 'Lax',
-        },
-    })
-);
+          },
+        })
+      );
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Middleware to parse JSON requests
-
-// Set Content Security Policy header
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", 
-        "default-src 'none'; img-src 'self' data:; script-src 'self'; style-src 'self';");
-    next();
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(flash());
 
 // ตั้งค่า EJS เป็น template engine
 app.use(expressLayouts);
-app.set('layout', 'layouts/main');
+app.set('layout', 'layouts/main'); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // กำหนด public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Routes setup
 app.use('/', require('./server/routes/authRoutes'));
